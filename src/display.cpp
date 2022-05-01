baf6402b097c7680444f92af9df2c3078dd0a2a8
Ian E <ian@teachable.com>
2022-04-30 20:18:43 -0600

    Create new icon

--- src/display.cpp
+++ src/display.cpp
@@ -3,9 +3,12 @@
 
 LiquidCrystal_I2C Display::lcd((PCF8574_address)0x27);
 
+byte gmail[] = {B00000, B00000, B10001, B11011, B10101, B10001, B00000, B00000};
+
 void Display::init()
 {
     lcd.begin(columns, rows);
+    lcd.createChar(0, gmail);
     lcd.noBacklight();
     lcd.setCursor(0, 0);
 }
